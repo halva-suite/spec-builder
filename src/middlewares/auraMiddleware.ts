b649795903794a94d1c48d5b4edd9861f699fc87
RTSTALER <rstaler@yandex.ru>
2020-06-24 16:38:31 +0700

    Fix non null checker

--- src/middlewares/auraMiddleware.ts
+++ src/middlewares/auraMiddleware.ts
@@ -2,7 +2,7 @@
 import HalvaMiddlewareContext from '../HalvaSpecModifier';
 
 export const auraMiddleware = (context: HalvaMiddlewareContext): any => {
-  if (!context.jsonSchema.genesis.runtime.aura.authorities) throw new Error('Invalid JSON');
+  if (!context.jsonSchema.genesis?.runtime?.aura?.authorities) throw new Error('Invalid JSON');
   const pairsAura = context.sr25519Keys.getPairs();
   for (let i = 0; i < pairsAura.length; i++) {
     context.jsonSchema.genesis.runtime.aura.authorities.push(pairsAura[i].address);
