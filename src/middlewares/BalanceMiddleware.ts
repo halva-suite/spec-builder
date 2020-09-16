3c383da30ab8449276ef5a98534b2fdbcd37fb08
staler <rtstaler@yandex.ru>
2020-09-16 18:15:30 +0700

    Update Middlewares

--- src/middlewares/BalanceMiddleware.ts
+++ src/middlewares/BalanceMiddleware.ts
@@ -2,7 +2,7 @@
 import HalvaMiddlewareContext from '../HalvaSpecModifier';
 
 export const balanceMiddleware = (context: HalvaMiddlewareContext): any => {
-  if (!context.jsonSchema.genesis?.runtime?.balances?.balances) throw new Error('Invalid JSON');
+  if (!context.jsonSchema.genesis?.runtime?.palletBalances?.balances) throw new Error('Invalid JSON');
   const pairsAura = context.sr25519Keys.getPairs();
   for (let i = 0; i < pairsAura.length; i++) {
     context.jsonSchema.genesis.runtime.balances.balances.push([pairsAura[i].address, context.balance]);
--- src/middlewares/GrandpaMiddleware.ts
+++ src/middlewares/GrandpaMiddleware.ts
@@ -2,7 +2,7 @@
 import HalvaMiddlewareContext from '../HalvaSpecModifier';
 
 export const grandpaMiddleware = (context: HalvaMiddlewareContext): any => {
-  if (!context.jsonSchema.genesis?.runtime?.grandpa?.authorities) throw new Error('Invalid JSON');
+  if (!context.jsonSchema.genesis?.runtime?.palletGrandpa?.authorities) throw new Error('Invalid JSON');
   const pairsGrandpa = context.ed25519Keys.getPairs();
   for (let i = 0; i < pairsGrandpa.length; i++) {
     context.jsonSchema.genesis.runtime.grandpa.authorities.push([pairsGrandpa[i].address, 1]);
